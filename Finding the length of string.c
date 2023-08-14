725f4598440927641751bfa2f87001095018d4f9
Swaroop5222 <141646466+Swaroop5222@users.noreply.github.com>
2023-08-14 12:32:16 +0530

    Add files via upload

---
+++ Finding the length of string.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+int main(){
+char str[100], i=0, length;
+printf("Enter the string:\n");
+gets(str);
+while(str[i]!='\0'){
+    i++;
+    length=i;
+}
+printf("The length of the string is %d",length);
+
+
+
+return 0;
+}
